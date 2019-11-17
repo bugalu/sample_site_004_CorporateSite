fccd950018e87db08654ff57ffa1557a84355683
ikeda-shin <ikeda-shin@office-chico.biz>
2019-11-17 19:49:10 +0900

    Implemented slideshow

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
-'use strict';
+"use strict";
 {
-  const title = document.getElementById('title');
+  const slide = document.getElementById("slide");
+  const img = new Array(
+    "./img/top-image1.jpg",
+    "./img/top-image2.jpg",
+    "./img/top-image3.jpg",
+    "./img/top-image4.jpg"
+  );
+  let count = -1;
+  slideShow();
+
+  function slideShow() {
+    count++;
+    if (count === img.length) {
+      count = 0;
+    }
+    slide.src = img[count];
+    slide.style.opacity = 1;
+    setTimeout(() => {
+      slide.style.opacity = 0;
+      setTimeout(() => {
+        slideShow();
+      }, 1500);
+    }, 7000);
+  }
 }
